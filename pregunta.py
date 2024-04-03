664cc787f14d341dd21a0d4745cb30f0f9c60163
nathalygarzon <nsgarzonr@unal.edu.co>
2024-04-03 16:31:55 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,30 +10,32 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
+    df = pd.read_csv("solicitudes_credito.csv", 
+                    sep=";")
     df = df.drop('Unnamed: 0', axis=1)
-    df = df.dropna()
 
     df['sexo'] = df['sexo'].str.lower()
-    df['sexo'] = df['sexo'].str.strip()
 
 
     df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
 
-
     df['idea_negocio'] = df['idea_negocio'].str.lower()
-    df['idea_negocio'] = df['idea_negocio'].str.replace("-","")
-    df['idea_negocio'] = df['idea_negocio'].str.replace("_","")
-    df['idea_negocio'] = df['idea_negocio'].str.strip()
+    df['idea_negocio'] = df['idea_negocio'].str.replace("-"," ")
+    df['idea_negocio'] = df['idea_negocio'].str.replace("_"," ")
 
-    df['barrio'] = df['barrio'].str.strip()
+
+    df['barrio'] = df['barrio'].str.lower()
+    df['barrio'] = df['barrio'].str.replace("-"," ")
+    df['barrio'] = df['barrio'].str.replace("_"," ")
+
 
     df['estrato'] = df['estrato'].astype(int)
 
     df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
 
-    df.fecha_de_beneficio= pd.to_datetime(df.fecha_de_beneficio, dayfirst = True, format='mixed')
+    df['fecha_de_beneficio']= pd.to_datetime(df['fecha_de_beneficio'],
+                                            dayfirst= True,
+                                            format='mixed')
 
     df['monto_del_credito'] = df['monto_del_credito'].str.strip("$")
     df['monto_del_credito'] = df['monto_del_credito'].str.replace(" ","")
@@ -41,9 +43,13 @@ def clean_data():
     df['monto_del_credito'] = df['monto_del_credito'].str.replace(",","")
 
     df['línea_credito'] = df['línea_credito'].str.lower()
-    df['línea_credito'] = df['línea_credito'].str.replace(".","")
-    df['línea_credito'] = df['línea_credito'].str.strip()
+    df['línea_credito'] = df['línea_credito'].str.replace("-"," ")
+    df['línea_credito'] = df['línea_credito'].str.replace("_"," ")
 
-    df = df.drop_duplicates()
+
+    df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
 
     return df
+
+
