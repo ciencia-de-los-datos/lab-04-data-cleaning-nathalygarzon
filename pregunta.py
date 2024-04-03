a41408912dd5c1ee72aefbf16c7363ca1d39281d
nathalygarzon <nsgarzonr@unal.edu.co>
2024-04-03 14:54:06 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -15,34 +15,34 @@ def clean_data():
     df = df.drop('Unnamed: 0', axis=1)
     df = df.dropna()
 
-    df["sexo"] = df["sexo"].str.lower()
-    df["sexo"] = df["sexo"].str.strip()
+    df['sexo'] = df['sexo'].str.lower()
+    df['sexo'] = df['sexo'].str.strip()
 
 
-    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
 
 
-    df["idea_negocio"] = df["idea_negocio"].str.lower()
-    df["idea_negocio"] = df["idea_negocio"].str.replace("-","")
-    df["idea_negocio"] = df["idea_negocio"].str.replace("_","")
-    df["idea_negocio"] = df["idea_negocio"].str.strip()
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].str.replace("-","")
+    df['idea_negocio'] = df['idea_negocio'].str.replace("_","")
+    df['idea_negocio'] = df['idea_negocio'].str.strip()
 
-    df["barrio"] = df["barrio"].str.strip()
+    df['barrio'] = df['barrio'].str.strip()
 
-    df["estrato"] = df["estrato"].astype(int)
+    df['estrato'] = df['estrato'].astype(int)
 
-    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
 
     df.fecha_de_beneficio= pd.to_datetime(df.fecha_de_beneficio, dayfirst = True, format='mixed')
 
-    df["monto_del_credito"] = df["monto_del_credito"].str.strip("$")
-    df["monto_del_credito"] = df["monto_del_credito"].str.replace(" ","")
-    df["monto_del_credito"] = df["monto_del_credito"].str.replace(".00","")
-    df["monto_del_credito"] = df["monto_del_credito"].str.replace(",","")
+    df['monto_del_credito'] = df['monto_del_credito'].str.strip("$")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(" ","")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(".00","")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(",","")
 
-    df["línea_credito"] = df["línea_credito"].str.lower()
-    df["línea_credito"] = df["línea_credito"].str.replace(".","")
-    df["línea_credito"] = df["línea_credito"].str.strip()
+    df['línea_credito'] = df['línea_credito'].str.lower()
+    df['línea_credito'] = df['línea_credito'].str.replace(".","")
+    df['línea_credito'] = df['línea_credito'].str.strip()
 
     df = df.drop_duplicates()
 
